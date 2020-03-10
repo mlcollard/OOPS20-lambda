5505a9739182e06eb23994edc1547f12eed1b6f4
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-03-10 08:43:02 -0400

    Add initial files

---
+++ lambda_x.cpp
@@ -0,0 +1,14 @@
+/*
+    lambda.cpp
+
+    Examples of captures in lambda functions
+*/
+
+#include <functional>
+#include <sstream>
+#include <assert.h>
+
+int main(int argc, char* argv[]) {
+
+    return 0;
+}
